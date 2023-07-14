82ba4b6a7a7b52c83b84f07934d83d2866f728f2
YadavYashvant <yashvantyadav855@gmail.com>
2023-07-15 01:26:51 +0530

    Typo Changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Alert from "./components/alert";
-import Button from "./components/button";
+import Button from "./components/Button";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
